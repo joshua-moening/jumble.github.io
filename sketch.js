87f0b201fa1db19a6de955a768465813d5d07f0a
Joshua Moening <joshua.moening.school@gmail.com>
2026-09-16 14:55:22 -0400

    Adjust button position in sketch.js

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
     wordInput = createInput();
     wordInput.position(CENTER, 65);
     button = createButton('Anagram');
-    button.position(CENTER, 80);
+    button.position(CENTER, 85);
     button.mousePressed(greet);
     wordInput.changed(greet);
 }
